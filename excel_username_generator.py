a244a41d36af1503165b3e0f097932f4dda03bf5
Prince.Bonsu <student.p.bonsu@horizoncolumnus.org>
2019-10-25 08:48:23 -0400

    work done

--- excel_username_generator.py
+++ excel_username_generator.py
@@ -1,1 +1,8 @@
 # do your magic here
+student_names = input(" First name")
+last= input(" Last name")
+
+for n in student_names:
+    print("student." + n[0] + "." + last + "@horizoncolumbus.org")
+
+  
