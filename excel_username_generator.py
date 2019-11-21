93726c6111297883be79f327c47b61ec5a7bc88c
Prince.Bonsu <student.p.bonsu@horizoncolumnus.org>
2019-11-21 07:59:56 -0500

    work done

--- excel_username_generator.py
+++ excel_username_generator.py
@@ -1,8 +1,17 @@
 # do your magic here
-student_names = input(" First name")
-last= input(" Last name")
 
-for n in student_names:
-    print("student." + n[0] + "." + last + "@horizoncolumbus.org")
+import xlrd
 
-  
+loc = ("HSA_Freshman_3010.xlsx")
+
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+# For row 0 and column 0
+
+
+for i in range(sheet.nrows):
+    fn = sheet.cell_value(i, 0)
+    ln = sheet.cell_value(i, 1)
+
+    print("student." + fn[0] + "." + ln + "@horizoncolumbus.org")
